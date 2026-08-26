b3aeeeba7d0a630a4940585d1e8ad35be3657f87
Hisham Ismaiel <hishamismaiel1975@outlook.com>
2026-08-26 21:01:36 +0300

    ADD GRPC Server Side

---
+++ Services/Discount/Discount.Grpc/Program.cs
@@ -0,0 +1,29 @@
+using Discount.Grpc.Services;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddGrpc();
+//For postman support
+builder.Services.AddGrpcReflection();
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(80, listenOptions =>
+    {
+        listenOptions.Protocols = HttpProtocols.Http2;
+    });
+});
+
+var app = builder.Build();
+
+app.MapGrpcService<DiscountGrpcService>();
+//For postman support
+app.MapGrpcReflectionService();
+
+
+
+app.MapGet("/", () =>
+    "This is a gRPC service. Use a gRPC client to communicate with it.");
+
+app.Run();
---
+++ Services/Discount/Discount.Grpc/Services/DiscountGrpcService.cs
@@ -0,0 +1,21 @@
+﻿using Grpc.Core;
+
+namespace Discount.Grpc.Services;
+
+public class DiscountGrpcService : DiscountService.DiscountServiceBase
+{
+    public override Task<GetDiscountResponse> GetDiscount(
+        GetDiscountRequest request,
+        ServerCallContext context)
+    {
+        var response = new GetDiscountResponse
+        {
+            ProductId = request.ProductId,
+            Amount = 10
+        };
+
+        return Task.FromResult(response);
+    }
+
+
+}
