81222a0c60c4e139adbf4c9515d039c261746905
Hisham Ismaiel <hishamismaiel1975@outlook.com>
2026-09-20 18:41:35 +0300

    Finished Gateway

--- Gateway/Gateway.api/Program.cs
+++ Gateway/Gateway.api/Program.cs
@@ -5,14 +5,22 @@ var builder = WebApplication.CreateBuilder(args);
 // Configure Kestrel
 builder.WebHost.ConfigureKestrel(options =>
 {
+    // Public HTTP API
     options.ListenAnyIP(80, listenOptions =>
     {
         listenOptions.Protocols = HttpProtocols.Http1;
     });
 
+    // Internal gRPC
     options.ListenAnyIP(81, listenOptions =>
     {
         listenOptions.Protocols = HttpProtocols.Http2;
+    });
+
+    // Internal/Admin HTTP
+    options.ListenAnyIP(8000, listenOptions =>
+    {
+        listenOptions.Protocols = HttpProtocols.Http1;
     });
 
 });
@@ -31,6 +39,24 @@ builder.Services
 
 
 var app = builder.Build();
+
+app.Use(async (context, next) =>
+{
+    var port = context.Connection.LocalPort;
+
+    var isAdminRoute =
+        context.Request.Path.StartsWithSegments("/portainer") ||
+        context.Request.Path.StartsWithSegments("/rabbitmq") ||
+        context.Request.Path.StartsWithSegments("/redisinsight");
+
+    if (isAdminRoute && port != 8000)
+    {
+        context.Response.StatusCode = StatusCodes.Status404NotFound;
+        return;
+    }
+
+    await next();
+});
 
 app.MapReverseProxy();
 
